1c148cfa998dd4f3916de18b8c23a6ef98242964
Jefri Herdi Triyanto <jefri.triyanto@ist.id>
2022-12-07 17:23:18 +0700

    add all router

--- router.js
+++ router.js
@@ -1,16 +1,17 @@
 const express = require("express");
 
 /**
+ * Router khusus FrontEnd
  * @param {express.Application} app
  */
 module.exports = (app) => {
-  app.get("/", (req, res) => res.render("index"));
-  app.get("/checkout", (req, res) => res.render("checkout"));
-  app.get("/dashboard", (req, res) => res.render("dashboard"));
-  app.get("/detail_product", (req, res) => res.render("detail_product"));
-  app.get("/keranjang", (req, res) => res.render("keranjang"));
-  app.get("/login", (req, res) => res.render("login"));
-  app.get("/products", (req, res) => res.render("products"));
-  app.get("/register", (req, res) => res.render("register"));
-  app.get("/wishlist", (req, res) => res.render("wishlist"));
+  app.get("/", (_, res) => res.render("index"));
+  app.get("/checkout", (_, res) => res.render("checkout"));
+  app.get("/dashboard", (_, res) => res.render("dashboard"));
+  app.get("/detail_product", (_, res) => res.render("detail_product"));
+  app.get("/keranjang", (_, res) => res.render("keranjang"));
+  app.get("/login", (_, res) => res.render("login"));
+  app.get("/products", (_, res) => res.render("products"));
+  app.get("/register", (_, res) => res.render("register"));
+  app.get("/wishlist", (_, res) => res.render("wishlist"));
 };
