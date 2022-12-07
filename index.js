64407e974be10b7ef2ea8c23e0d878d171190f8b
herfansyaadiyoce <herfansyaadiyoce@gmail.com>
2022-12-07 14:59:27 +0700

    menambahkan js

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+const app = express();
+app.set("port", 8080);
+app.listen(app.get("port"), console.log("Listen at " + app.get("port")));
+
+// Middlewares
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  return res.send("mantap cuk...");
+});
+
+app.all("*", (req, res) => {
+  return res.send("page not found...");
+});
