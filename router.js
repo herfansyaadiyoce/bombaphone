6adb42ad6405e4141a4337cf51e1f29bd8611294
Jefri Herdi Triyanto <jefri.triyanto@ist.id>
2022-12-08 20:53:55 +0700

    add new template

--- router.js
+++ router.js
@@ -5,12 +5,15 @@ const express = require("express");
  * @param {express.Application} app
  */
 module.exports = (app) => {
-  app.get("/", (_, res) => res.render("index"));
+  app.get("/checkout-payment", (_, res) => res.render("checkout-payment"));
+  app.get("/checkout-shipping", (_, res) => res.render("checkout-shipping"));
   app.get("/checkout", (_, res) => res.render("checkout"));
   app.get("/dashboard", (_, res) => res.render("dashboard"));
-  app.get("/detail_product", (_, res) => res.render("detail_product"));
+  app.get("/detail-product", (_, res) => res.render("detail-product"));
+  app.get("/", (_, res) => res.render("index"));
   app.get("/keranjang", (_, res) => res.render("keranjang"));
   app.get("/login", (_, res) => res.render("login"));
+  app.get("/lupa-password", (_, res) => res.render("lupa-password"));
   app.get("/products", (_, res) => res.render("products"));
   app.get("/register", (_, res) => res.render("register"));
   app.get("/wishlist", (_, res) => res.render("wishlist"));
